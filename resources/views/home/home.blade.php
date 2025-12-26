1ea357bd0022a9c5e345487f58d312401aacd741
Nguyễn Anh Huy <nguyenhuy140923@gmail.com>
2025-12-27 00:22:48 +0700

    Add home page layout with header and navigation links

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <style>
+    
+    
+    </style>
+</head>
+<body>
+    <div class="home">
+    <header class="header">
+    <div class="xaiv">
+    <a href="" class="seller-channel">Kênh người bán</a>
+    <a href="" class="download-app">Tải ứng dụng</a>
+    <a href="" class="connect">kết nối<i class="fa-brands fa-facebook-f"></i></a>
+    <a href="" class="Notification"><i class="">Thông báo</i></a>
+    
+
+    </div>
+    
+    </header>
+    
+    </div>
+</body>
+</html>
