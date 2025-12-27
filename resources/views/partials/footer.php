2b6a4da403631c00f2a12ead528a24302caad943
Nguyễn Anh Huy <nguyenhuy140923@gmail.com>
2025-12-27 16:19:53 +0700

    Update AuthController to reference the correct login view and refactor footer into a separate partial

---
+++ resources/views/partials/footer.php
@@ -0,0 +1,72 @@
+    <main class="footer">
+        <div class="footer-container">
+            <div class="service">
+                <h3>DỊCH VỤ KHÁCH HÀNG</h3>
+                <ul>
+                    <li><a href="#">Trung Tâm Trợ Giúp Uni</a></li>
+                    <li><a href="#">Hướng Dẫn Mua Hàng/Đặt Hàng</a></li>
+                    <li><a href="#">Hướng Dẫn Bán Hàng</a></li>
+                    <li><a href="#">Đơn Hàng</a></li>
+                    <li><a href="#">Trả Hàng/Hoàn Tiền</a></li>
+                    <li><a href="#">Liên Hệ Uni</a></li>
+                    <li><a href="#">Chính Sách Bảo Hành</a></li>
+                </ul>
+            </div>
+            <div class="pay">
+                <h3>THANH TOÁN</h3>
+                <ul>
+                    <li><a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg"
+                                alt="Visa"></a></li>
+                    <li><a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
+                                alt="Mastercard"></a></li>
+                    <li><a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/4/40/JCB_logo.svg"
+                                alt="JCB"></a></li>
+                </ul>
+            </div>
+            <div class="monitor">
+                <h3>THEO DÕI UNIMARKET</h3>
+                <ul>
+                    <li>
+                        <i class="fa-brands fa-facebook"></i>
+                        <a href="#">Facebook</a>
+                    </li>
+                    <li>
+                        <i class="fa-brands fa-square-instagram"></i>
+                        <a href="#">Instagram</a>
+                    </li>
+                    <li>
+                        <i class="fa-brands fa-linkedin"></i>
+                        <a href="#">LinkedIn</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="download">
+                <h3>TẢI ỨNG DỤNG UNIMARKET</h3>
+                <div class="download-content">
+                    <!-- <div class="qr-code">
+                        <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://unimarket.local"
+                            alt="QR Code"> -->
+                    </div>
+                    <div class="store-links">
+                        <a href="#">
+                            <img src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+                                alt="App Store">
+                        </a>
+                        <a href="#">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
+                                alt="Google Play">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <div class="copyright">
+        <div class="copyright-container">
+            <p class="copyright-text">© 2025 UniMarket. Tất cả các quyền được bảo lưu.</p>
+            <div class="country-section">
+                <p class="country-label">Quốc gia & Khu vực:</p>
+                <p class="country-value">Việt Nam</p>
+            </div>
+        </div>
+    </div>
